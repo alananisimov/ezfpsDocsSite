3bc31e53e0f554a4feab5ed9bf81463052912a25
alananisimov <alananisimov@gmail.com>
2023-10-01 22:44:50 +0300

    f

---
+++ app.config.ts
@@ -0,0 +1,26 @@
+export default defineAppConfig({
+  docus: {
+    title: "ezfps",
+    description: "The best place to start your documentation.",
+    image:
+      "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
+    socials: {
+      github: "alananisimov/ezfpsDocsSite",
+    },
+    aside: {
+      level: 0,
+      exclude: [],
+    },
+    header: {
+      title: "ezfps docs",
+      logo: false,
+    },
+    footer: {
+      credits: {
+        icon: "",
+        text: "ezfps docs",
+        href: "/",
+      },
+    },
+  },
+});
